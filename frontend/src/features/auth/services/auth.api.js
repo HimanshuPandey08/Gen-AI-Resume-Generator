4e0aca0d216c2f09475f75d8c0f140b4cd24a3cf
Himanshu Pandey <pandeyhimanshu6394@gmail.com>
2026-08-01 11:42:23 +0530

    adding .env file for port

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    // baseURL:`http://localhost:${process.env.PORT}`,
-    baseURL:`http://localhost:3000`,
+    baseURL: import.meta.env.VITE_API_URL,
+    // baseURL:`http://localhost:3000`,
     withCredentials:true 
 })
 
